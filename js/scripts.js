522c653469f58005d2b654f9898c0dd7d54c952c
julia sheremet and Susan Southall <susan@>
2018-03-26 10:32:53 -0700

    Add new array and append in a list form

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,25 @@ $(document).ready(function(){
 
     var name = $("input#name").val();
     var favoriteFood = $("input#favFood").val();
+    var favoriteDrink = $("input#favDrink").val();
     var favoriteColor = $("input#favColor").val();
     var favoriteAnimal = $("input#favAnimal").val();
     debugger;
-    var results = [name, favoriteFood, favoriteColor, favoriteAnimal];
+    var results = [name, favoriteFood, favoriteDrink, favoriteColor, favoriteAnimal];
+
+    var resultsCloned = results.slice().reverse().join(", ");
+
+    var newArray = [];
+    var resultsClonedAgain = results.slice(0,3).join(", ");
+    newArray.push(resultsClonedAgain);
+    results = results.join(", ");
+
+    //turns our array into 2 elements
 
     // results.toString();
     $("#user-output").text(results);
+    $("#user-output-reversed").text(resultsCloned);
+    $("ul#user-output-three-elements").append("<li>" + newArray + "</li>"); //resultsClonedArray
 
   });
 });
